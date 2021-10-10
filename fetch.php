81a6f8fa9988a1f9681b0bc993b7d4e0ae5f404e
Anthony R <4000degrees@gmail.com>
2021-10-10 17:59:09 +0300

    added fetching for sitemap index solved forbidden issue

--- fetch.php
+++ fetch.php
@@ -2,10 +2,16 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
 if (isset($_GET['url'])) {
     $url = $_GET['url'];
     $responce = [];
+    $context = stream_context_create(
+        array(
+        "http" => array(
+            "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+        )
+    )
+    );
     if (($data = @file_get_contents($url)) === false) {
         $error = error_get_last();
         $responce["ok"] = 0;
